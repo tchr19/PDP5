d98089fcf8bccff25754a28b24abbb02b9bc5fbe
tchr19 <81689897+tchr19@users.noreply.github.com>
2022-11-29 12:06:35 +0100

    Add files via upload

---
+++ Untitled3.m
@@ -0,0 +1,34 @@
+serialportlist("available")
+
+
+arduinoObj = serialport("COM3",9600)
+configureTerminator(arduinoObj,"CR/LF");
+flush(arduinoObj);
+arduinoObj.UserData = struct("Data",[],"Count",1, "Direction", "")
+
+configureCallback(arduinoObj,"terminator",@readEncoderData);
+
+function readEncoderData(src, ~)
+    % Read the ASCII data from the serialport object.
+    data = readline(src)
+    %newStr = split(data,"|")
+    %new2 = split(newStr(2),":")
+    % Convert the string data to numeric type and save it in the UserData
+    % property of the serialport object.
+    src.UserData.Data(end+1) = str2double(data);
+
+    % Update the Count value of the serialport object.
+    src.UserData.Count = src.UserData.Count + 1;
+
+    % If 1001 data points have been collected from the Arduino, switch off the
+    % callbacks and plot the data.
+    
+    if src.UserData.Count > 1001
+        configureCallback(src, "off");
+        %data
+        plot(src.UserData.Data(2:end));
+    end
+end
+
+
+
